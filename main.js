e834d27c59a495b15cc7a34f6c23621ff91a5203
87werner <twerner15@googlemail.com>
2021-04-20 10:16:45 +0100

    sounds added and logic to play sounds

---
+++ main.js
@@ -0,0 +1,36 @@
+//Drum sounds
+const boom = document.getElementById("boom")
+const clap = document.getElementById("clap")
+const hihat = document.getElementById("hihat")
+const kick = document.getElementById("kick")
+const openHat = document.getElementById("openhat")
+const ride = document.getElementById("ride")
+const snare = document.getElementById("snare")
+const tink = document.getElementById("tink")
+const tom = document.getElementById("tom")
+
+console.log(boom)
+
+document.addEventListener("keydown", (e) => {
+    console.log(e)
+    console.log(e.keyCode)
+    if(e.which == 65){
+        boom.play()
+    }else if(e.which == 83){
+        clap.play()
+    } else if(e.which == 68){
+        
+    }else if(e.which == 70){
+
+    } else if(e.which == 71){
+
+    }else if(e.which == 72){
+
+    }else if(e.which == 74){
+
+    }else if(e.which == 75){
+
+    }else if(e.which == 76)
+    
+    
+})
